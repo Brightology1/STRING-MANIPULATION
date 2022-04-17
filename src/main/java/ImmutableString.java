45b2a232057a6c434a9fd13f774d4d6ffc257772
Brightology1 <bright.anokwuru@decagon.dev>
2022-04-17 01:30:47 +0100

    Immutable String

---
+++ src/main/java/ImmutableString.java
@@ -0,0 +1,9 @@
+public class ImmutableString {
+    public static void main(String[] args) {
+        // Immutable String
+        String str = "Hello";
+        System.out.println(str);
+        str = str + " World";
+        System.out.println(str);
+    }
+}
