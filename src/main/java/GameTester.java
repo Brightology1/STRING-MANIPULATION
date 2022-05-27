d3b8033718568cd042bb581962e8a4110f064fec
Brightology1 <bright.anokwuru@decagon.dev>
2022-05-27 07:28:27 +0100

    GameTester

---
+++ src/main/java/GameTester.java
@@ -0,0 +1,7 @@
+public class GameTester {
+    public static void main(String[] args) {
+        GuesserGame game = new GuesserGame();
+        game.guessNum();
+        game.checkGuess(50);
+    }
+}
--- src/main/java/GuesserGame.java
+++ src/main/java/GuesserGame.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
 
 public class GuesserGame {
+
     int gnum;
-    
     public int guessNum(){
         System.out.println("Guesser! Kindly guess a number between 1 to 100");
         Scanner scan = new Scanner(System.in);
         gnum = scan.nextInt();
         return gnum;
     }
+public void checkGuess(int gnum){
+        if(gnum == 50){
+            System.out.println("You guessed it right!");
+        }
+        else if(gnum < 50){
+            System.out.println("Your guess is too low");
+        }
+        else{
+            System.out.println("Your guess is too high");
+        }
+    }
+
+
 }
