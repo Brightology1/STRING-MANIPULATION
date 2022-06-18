d68cfc97c6c00ce40345a7ff566c6c0d81910140
Brightology1 <bright.anokwuru@decagon.dev>
2022-06-19 00:33:57 +0100

    Non Rep Char

--- src/main/java/GameTester.java
+++ src/main/java/GameTester.java
@@ -1,7 +1,13 @@
+import java.util.logging.Logger;
+
 public class GameTester {
+  // static Logger logger = Logger.getLogger(GameTester.class.getName());
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(GameTester.class.getName());
         GuesserGame game = new GuesserGame();
         game.guessNum();
         game.checkGuess(50);
+        logger.info("Game over");
+        //logger.log(LEVEL.INFO, "The Game is over");
     }
 }
---
+++ src/main/java/MaxSwap.java
@@ -0,0 +1,5 @@
+public class MaxSwap {
+    public String maximiser(String input1, int input2){
+        return "";
+    }
+}
